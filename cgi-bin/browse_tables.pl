fe2056592dc4aba2f2caf3d4179168c5c14622ea
Don Mahurin <dm@openright.org>
2002-06-29 00:25:52 +0000

    inentation using tabs

--- cgi-bin/browse_tables.pl
+++ cgi-bin/browse_tables.pl
@@ -13,10 +13,10 @@ package browse_tables;
 
 sub show_page
 {
- my($path)=@_;
-my $head_tags = view::get_style_head_tags();
+	my($path)=@_;
+	my $head_tags = view::get_style_head_tags();
 
-print <<"END";
+	print <<"END";
 <HTML>
 <head>
 $head_tags
@@ -24,54 +24,54 @@ $head_tags
 <BODY class="topics-back">
 END
 
-   view::read_page_template();
-   print $view::define::page_header if(defined($view::define::page_header));
-show($path);
-   print $view::define::page_footer if(defined($view::define::page_footer));
+	view::read_page_template();
+	print $view::define::page_header if(defined($view::define::page_header));
+	show($path);
+	print $view::define::page_footer if(defined($view::define::page_footer));
 
-print "</BODY>\n";
-print "</HTML>\n";
+	print "</BODY>\n";
+	print "</HTML>\n";
 }
 
 
 sub show
 {
-my($notes_path) = @_;
-   my($css_tables) = css_tables->new();
-unless( auth::check_current_user_file_auth( 'r', $notes_path ) )
-{
-   print "You are not authorized to access this path.\n";
-   return(0);
-}
-print_main_topic_table($notes_path, $css_tables);
-print "<br>";
-
-
-my $dfile = filedb::default_file($notes_path);
-for my $file (filedb::get_directory_list($notes_path))
-{
-	next if($file eq $dfile);
-	if( $file =~ m:^([^/]*)$: ) # untaint dir entry
-        {
-		$file = $1;
-	}
-	else
+	my($notes_path) = @_;
+	my($css_tables) = css_tables->new();
+	unless( auth::check_current_user_file_auth( 'r', $notes_path ) )
 	{
-		print "hey, /'s ? not good.\n";
-                return(0);
+		print "You are not authorized to access this path.\n";
+		return(0);
 	}
-	print_topic_table( "$notes_path/$file", $css_tables);	
-}
+	print_main_topic_table($notes_path, $css_tables);
+	print "<br>";
 
-unless(view::get_view_mode("superlayout") eq "framed")
-{
-	print $css_tables->table_begin("topic-table") . "\n";
-	print $css_tables->trtd_begin("topic-actions") . "\n";
-	view::actions3($notes_path);
-	print $css_tables->trtd_end();
-	print $css_tables->table_end() . "\n";
-}
-return 1;
+
+	my $dfile = filedb::default_file($notes_path);
+	for my $file (filedb::get_directory_list($notes_path))
+	{
+		next if($file eq $dfile);
+		if( $file =~ m:^([^/]*)$: ) # untaint dir entry
+		{
+			$file = $1;
+		}
+		else
+		{
+			print "hey, /'s ? not good.\n";
+			return(0);
+		}
+		print_topic_table( "$notes_path/$file", $css_tables);
+	}
+
+	unless(view::get_view_mode("superlayout") eq "framed")
+	{
+		print $css_tables->table_begin("topic-table") . "\n";
+		print $css_tables->trtd_begin("topic-actions") . "\n";
+		view::actions3($notes_path);
+		print $css_tables->trtd_end();
+		print $css_tables->table_end() . "\n";
+	}
+	return 1;
 }
 
 
--- cgi-bin/browse_tabs.pl
+++ cgi-bin/browse_tabs.pl
@@ -12,100 +12,100 @@ require 'view_lib.pl';
 
 sub show_page
 {
-   my($path) = @_;
+	my($path) = @_;
 
-my $head_tags = view::get_style_head_tags();
+	my $head_tags = view::get_style_head_tags();
 
-print <<"END";
+	print <<"END";
 <HTML>
 <head>
 $head_tags
 </head>
 <BODY class="topics-back">
 END
-   view::read_page_template();
-   print $view::define::page_header if(defined($view::define::page_header));
-    show($path);
-   print $view::define::page_footer if(defined($view::define::page_footer));
-print "</BODY>\n";
-print "</HTML>\n";
+	view::read_page_template();
+	print $view::define::page_header if(defined($view::define::page_header));
+	show($path);
+	print $view::define::page_footer if(defined($view::define::page_footer));
+	print "</BODY>\n";
+	print "</HTML>\n";
 }
 
 sub show
 {
-   my($notes_path) = @_;
-   my($css_tables) = css_tables->new();
-   unless( auth::check_current_user_file_auth( 'r', $notes_path ) )
-   {
-      print "You are not authorized to access this path.\n";
-      return(0);
-   }
+	my($notes_path) = @_;
+	my($css_tables) = css_tables->new();
+	unless( auth::check_current_user_file_auth( 'r', $notes_path ) )
+	{
+		print "You are not authorized to access this path.\n";
+		return(0);
+	}
 
-$notes_path =~ m:([^/]*)$:;
-my($notes_name) = $1;
+	$notes_path =~ m:([^/]*)$:;
+	my($notes_name) = $1;
 
-print $css_tables->table_begin("topic-table") . "\n";
-
-print $css_tables->trtd_begin("topic-title") . "\n";
-view::print_icon_img($notes_path);
-print "<b>$notes_name</b>\n";
-print $css_tables->trtd_end() . "\n";
-
-print $css_tables->trtd_begin("topic-info") . "\n";
-view::print_modification($notes_path);
-print $css_tables->trtd_end() . "\n";
-
-print $css_tables->trtd_begin("topic-text") . "\n";
-print "&nbsp;" unless (&view::print_dir_file($notes_path));
-print $css_tables->trtd_end() . "\n";
-
-print $css_tables->trtd_begin("topic-actions") . "\n";
-view::actions2($notes_path);
-print $css_tables->trtd_end() . "\n";
-
-print $css_tables->trtd_begin("topic-listing") . "\n";
-print "<table border=1><tr>\n";
-my @dirs;
-my $dfile = filedb::default_file($notes_path);
-for my $file (filedb::get_directory_list($notes_path))
-{
-   if( filedb::is_dir($notes_path, $file))
-   {
-       push(@dirs, $file);
-       next;
-   }
-   next if($file eq $dfile);
-
-   print "<td>\n";
-   next unless(view::print_link_html( "$notes_path/$file"));
-   print "</td>\n";
-}
-if(@dirs)
-{
-   for my $dir (@dirs)
-   {
-   print "<td>\n";
-     view::print_link_html( "$notes_path/$dir");
-   print "</td>\n";
-   }
-}
-print "</tr></table>\n";
-   
-print $css_tables->trtd_end() . "\n";
-
-print $css_tables->table_end() . "\n";
-
-unless(view::get_view_mode("superlayout") eq "framed")
-{
 	print $css_tables->table_begin("topic-table") . "\n";
-	print $css_tables->trtd_begin("topic-actions") . "\n";
-	view::actions3($notes_path);
-	print $css_tables->trtd_end() . "\n";
-	print $css_tables->table_end() . "\n";
-}
 
-#view::log($notes_path);
-return 1;
+	print $css_tables->trtd_begin("topic-title") . "\n";
+	view::print_icon_img($notes_path);
+	print "<b>$notes_name</b>\n";
+	print $css_tables->trtd_end() . "\n";
+
+	print $css_tables->trtd_begin("topic-info") . "\n";
+	view::print_modification($notes_path);
+	print $css_tables->trtd_end() . "\n";
+
+	print $css_tables->trtd_begin("topic-text") . "\n";
+	print "&nbsp;" unless (&view::print_dir_file($notes_path));
+	print $css_tables->trtd_end() . "\n";
+
+	print $css_tables->trtd_begin("topic-actions") . "\n";
+	view::actions2($notes_path);
+	print $css_tables->trtd_end() . "\n";
+
+	print $css_tables->trtd_begin("topic-listing") . "\n";
+	print "<table border=1><tr>\n";
+	my @dirs;
+	my $dfile = filedb::default_file($notes_path);
+	for my $file (filedb::get_directory_list($notes_path))
+	{
+		if( filedb::is_dir($notes_path, $file))
+		{
+			push(@dirs, $file);
+			next;
+		}
+		next if($file eq $dfile);
+
+		print "<td>\n";
+		next unless(view::print_link_html( "$notes_path/$file"));
+		print "</td>\n";
+	}
+	if(@dirs)
+	{
+		for my $dir (@dirs)
+		{
+			print "<td>\n";
+			view::print_link_html( "$notes_path/$dir");
+			print "</td>\n";
+		}
+	}
+	print "</tr></table>\n";
+
+	print $css_tables->trtd_end() . "\n";
+
+	print $css_tables->table_end() . "\n";
+
+	unless(view::get_view_mode("superlayout") eq "framed")
+	{
+		print $css_tables->table_begin("topic-table") . "\n";
+		print $css_tables->trtd_begin("topic-actions") . "\n";
+		view::actions3($notes_path);
+		print $css_tables->trtd_end() . "\n";
+		print $css_tables->table_end() . "\n";
+	}
+
+	#view::log($notes_path);
+	return 1;
 }
 
 1;
