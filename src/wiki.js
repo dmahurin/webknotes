eeae83c2b486898cd2102e44deda6822bbc9fbb8
Don Mahurin <dm@openright.org>
2010-08-24 18:21:41 +0000

    Default to .wiki for references without extension. Strip <pre> out of body (pre required due to IE bug).

--- src/wiki.js
+++ src/wiki.js
@@ -62,7 +62,7 @@ var Wiky = {
        { rex:/\(-(.+?)-\)/g, tmplt:"<del>$1</del>" },
        { rex:/\?([^ \t\f\v\xB6]+)\((.+)\)\?/g, tmplt:"<abbr title=\"$2\">$1</abbr>" },  // .. abbreviation ..
        //{ rex:/\[\[([^ ,]+)[, ]([^\]]*)\]\]/g, tmplt:"<a href=\"$1\">$1</a>" },
-       { rex:/\[\[([^ ,\]]+)([, \]]([^\]]*))?\]\]/g, tmplt:function($0,$1,$2){return Wiky.store("<a href=\""+$1+"\">"+($2?$3:$1)+"</a>"); } },
+       { rex:/\[\[([^ ,\]]+)([, \]]([^\]]*))?\]\]/g, tmplt:function($0,$1,$2){var ref=$1; if(null == ref.match(/\./)) ref = ref + ".wiki"; return Wiky.store("<a href=\""+ref+"\">"+($2?$3:$1)+"</a>"); } },
        { rex:/\[(?:\{([^}]*)\})?[Ii]ma?ge?\:([^ ,\]]*)(?:[, ]([^\]]*))?\]/g, tmplt:function($0,$1,$2,$3){return Wiky.store("<img"+Wiky.style($1)+" src=\""+$2+"\" alt=\""+($3?$3:$2)+"\" title=\""+($3?$3:$2)+"\"/>");} },  // wikimedia image style ..
        { rex:/\[([^ ,]+)[, ]([^\]]*)\]/g, tmplt:function($0,$1,$2){return Wiky.store("<a href=\""+$1+"\">"+$2+"</a>");}},  // wiki block style uri's ..
        { rex:/(((http(s?))\:\/\/)?[A-Za-z0-9\._\/~\-:]+\.(?:png|jpg|jpeg|gif|bmp))/g, tmplt:function($0,$1,$2){return Wiky.store("<img src=\""+$1+"\" alt=\""+$1+"\"/>");} },  // simple images .. 
@@ -377,7 +377,11 @@ var Wiky = {
 // auto convert added to original wiky.js
 function convert()
 {
-        document.body.innerHTML = Wiky.toHtml(document.body.innerHTML);
+	var text = document.body.innerHTML;
+ 
+	text = text.replace(/^<pre>/,'');
+	text = text.replace(/<\/pre>$/,'');
+        document.body.innerHTML = Wiky.toHtml(text);
 }
 
 window.onload = convert;
